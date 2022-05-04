b2ea7e07096bc314eaf012a054c19bef3c6426b7
Benneth-Bejamas <benneth@bejamas.io>
2022-05-04 19:30:50 +0100

    creating the signup route

---
+++ spotify/pages/api/signup.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import bcrypt from "bcrypt";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+import prisma from "../../lib/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: await bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    res.status(401).json({ message: "Email already in use" });
+    console.log(e.message);
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    "hello",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60 * 1000,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  // send a user to frontend
+  res.json(user);
+};
--- spotify/prisma/seed.ts
+++ spotify/prisma/seed.ts
@@ -35,6 +35,23 @@ const run = async () => {
       password: await bcrypt.hashSync("password", salt),
     },
   });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(0).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist ${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({ id: song.id })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
